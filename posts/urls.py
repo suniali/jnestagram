0225c7825ea71824f03847d0150c9b2769e61667
Jarvis <alimatroky@gmail.com>
2025-12-22 13:29:53 +0330

    add Create post

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PostListView,like_post
+from .views import PostListView,like_post,PostCreateView
 
 urlpatterns = [
     path('',PostListView.as_view(),name='home'),
     path('like/<int:post_id>/', like_post, name='like_post'),
+    path('create/', PostCreateView.as_view(), name='create_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView
-from django.db.models import Q,Count
-from django.shortcuts import get_object_or_404,redirect
 from django.views.decorators.http import require_POST
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView,CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q,Count
 from django.http import JsonResponse
+from django.urls import reverse_lazy
 
 from .models import Post, Tag,Like
+from .form import PostForm
 
 class PostListView(ListView):
     model = Post
@@ -43,6 +46,31 @@ class PostListView(ListView):
         context['top_posts']= top_posts.order_by('-score')[:4]
 
         return context
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model=Post
+    form_class=PostForm
+    template_name="posts/post.html"
+    success_url=reverse_lazy('home')
+    
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context['tags']=Tag.objects.all()
+        return context
+    
+    def form_valid(self, form):
+        form.instance.user=self.request.user
+        tag_ids_str=self.request.POST.get('selected_tags','')
+        response=super().form_valid(form)
+        
+        if tag_ids_str:
+            try:
+                tags=[int(tid) for tid in tag_ids_str.split(',') if tid.strip().isdigit()]
+                self.object.tag.set(tags)
+            except ValueError:
+                pass
+        return response
+        
 
 @require_POST
 @login_required
