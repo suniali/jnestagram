55d9c4939c8c7f71d0d44ce4c747b96b3a480369
Jarvis <alimatroky@gmail.com>
2025-12-28 16:16:17 +0330

    Fix delete condition replay & better like signal for generic Like

--- posts/signals.py
+++ posts/signals.py
@@ -1,20 +1,25 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.db.models import F
 
-from .models import Post,Comment,Like
+from .models import Comment,Like
 
 @receiver(post_save, sender=Like)
 def update_like_count_on_save(sender, instance, created,**kwargs):
-    if created:
-        instance.content_object.likes_count+=1
-        instance.content_object.save(update_fields=['likes_count'])
+    obj=instance.content_object
+
+    if created and obj and hasattr(obj,'likes_count'):
+        obj.likes_count=F('likes_count')+1
+        obj.save(update_fields=['likes_count'])
 
 @receiver(post_delete, sender=Like)
 def update_like_count_on_delete(sender, instance,**kwargs):
-    if instance.content_object.likes_count >0:
-        instance.content_object.likes_count-=1
-        instance.content_object.save(update_fields=['likes_count'])
+    obj=instance.content_object
 
+    if obj and hasattr(obj,'likes_count'):
+        if obj.likes_count > 0:
+            obj.likes_count=F('likes_count')-1
+            obj.save(update_fields=['likes_count'])
 @receiver(post_save, sender=Comment)
 def update_comments_count_on_save(sender, instance, created, **kwargs):
     if created:
