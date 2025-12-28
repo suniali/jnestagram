09bbe5d27ea9fb1da5742512181f298f14f81cd9
Jarvis <alimatroky@gmail.com>
2025-12-28 14:12:38 +0330

    fix issue on context_processors.py

--- posts/context_processors.py
+++ posts/context_processors.py
@@ -2,7 +2,7 @@ from .models import Comment
 
 def pending_comments_count(request):
     if request.user.is_authenticated:
-        count=Comment.objects.filter(user=request.user,is_approved=False).count()
+        count=Comment.objects.filter(post__user=request.user,is_approved=False).count()
         return  {'pending_comments_count':count}
     else:
         return {'pending_comments_count':0}
