f78ca86b034619fa9295496e8243db696b03110a
Jarvis <alimatroky@gmail.com>
2025-12-29 22:53:18 +0330

    solve selected category issue

--- posts/templatetags/sidebar.py
+++ posts/templatetags/sidebar.py
@@ -5,8 +5,8 @@ from posts.models import Tag,Post
 register = Library()
 
 @register.inclusion_tag('partials/sidebar.html')
-def sidebar_view():
+def sidebar_view(current_tag=None):
     tags=Tag.objects.all()
     top_posts=Post.objects.filter(is_active=True, is_public=True,likes_count__gt=0).order_by('-likes_count','-comments_count')[:4]
-    context={'tags':tags,'top_posts':top_posts}
+    context={'tags':tags,'top_posts':top_posts,'current_tag':current_tag}
     return context
