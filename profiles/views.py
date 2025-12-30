6071f384a431075b29bdc2cf0e6e5dbd9826356a
Jarvis <alimatroky@gmail.com>
2025-12-30 15:41:19 +0330

    change public profile to ajax

--- profiles/views.py
+++ profiles/views.py
@@ -242,13 +242,10 @@ class PublicProfileView(DetailView):
 
     def get_queryset(self):
         queryset=Profile.objects.select_related('user','country')
-
-        posts=Post.objects.filter(is_active=True).order_by('-created_at')
-
-        top_posts=Post.objects.filter(user__profile__in=queryset,is_active=True).order_by('-likes_count')[:5]
-
-        top_comments=Comment.objects.filter(is_approved=True).order_by('-likes_count','-replays_count')[:5]
-        replays=Replay.objects.select_related('user')
+        posts_qs=Post.objects.filter(is_active=True).order_by('-created_at')
+        top_posts_qs=Post.objects.filter(is_active=True).order_by('-likes_count')[:5]
+        top_comments_qs=Comment.objects.filter(is_approved=True).order_by('-likes_count','-replays_count')[:5]
+        replays_qs=Replay.objects.select_related('user')
 
         if self.request.user.is_authenticated:
             post_type=ContentType.objects.get_for_model(Post)
@@ -269,24 +266,48 @@ class PublicProfileView(DetailView):
                 content_type=replay_type,
                 object_id=Cast(OuterRef('pk'),CharField())
             )
-            posts=posts.annotate(is_liked=Exists(user_post_likes))
-            top_comments=top_comments.annotate(is_liked=Exists(user_comment_likes))
-            replays=replays.annotate(is_liked=Exists(user_replay_likes))
+            posts_qs=posts_qs.annotate(is_liked=Exists(user_post_likes))
+            top_comments_qs=top_comments_qs.annotate(is_liked=Exists(user_comment_likes))
+            replays_qs=replays_qs.annotate(is_liked=Exists(user_replay_likes))
         else:
-            posts=posts.annotate(is_liked=Value(False,output_field=BooleanField()))
-            top_comments=top_comments.annotate(is_liked=Value(False,output_field=BooleanField()))
-            replays=replays.annotate(is_liked=Value(False,output_field=BooleanField()))
+            posts_qs=posts_qs.annotate(is_liked=Value(False,output_field=BooleanField()))
+            top_comments_qs=top_comments_qs.annotate(is_liked=Value(False,output_field=BooleanField()))
+            replays_qs=replays_qs.annotate(is_liked=Value(False,output_field=BooleanField()))
 
-        top_comments=top_comments.prefetch_related(
-                Prefetch('replays', queryset=replays, to_attr='replies'),
+        top_comments_qs=top_comments_qs.prefetch_related(
+                Prefetch('replays', queryset=replays_qs, to_attr='replies'),
         )
 
-        return queryset.prefetch_related(
-            Prefetch('user__posts',queryset=posts,to_attr='all_posts'),
-            Prefetch('user__posts',queryset=top_posts,to_attr='top_posts'),
-            Prefetch('user__comments',queryset=top_comments,to_attr='top_comments'),
+        queryset= queryset.prefetch_related(
+            Prefetch('user__posts',queryset=posts_qs,to_attr='all_posts'),
+            Prefetch('user__posts',queryset=top_posts_qs,to_attr='top_posts'),
+            Prefetch('user__comments',queryset=top_comments_qs,to_attr='top_comments'),
         )
+        return queryset
 
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+
+        obj=self.object
+        if 'top-posts' in self.request.GET:
+            context['posts']=obj.user.top_posts
+        elif 'top-comments' in self.request.GET:
+            context['top_comments']=obj.user.top_comments
+        else:
+            context['posts']=obj.user.all_posts
+
+        return context
+
+    def render_to_response(self, context, **response_kwargs):
+        if self.request.htmx:
+            if 'top-posts' in self.request.GET:
+                return render(self.request, 'partials/posts/posts_container.html', {'posts':context['posts']})
+            elif 'top-comments' in self.request.GET:
+                return render(self.request, 'partials/posts/comments_container.html', {'display_comments':context['top_comments']})
+            else:
+                return render(self.request, 'partials/posts/posts_container.html', {'posts': context['posts']})
+
+        return super().render_to_response(context, **response_kwargs)
 class UserDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = User
     template_name = 'layouts/generic_delete.html'
