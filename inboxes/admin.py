ffe0a40bfbc327ae2b903aa4e7b47deeea2a5a8f
Jarvis <alimatroky@gmail.com>
2026-01-03 19:04:25 +0330

    read only message field for admin

--- inboxes/admin.py
+++ inboxes/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import Conversation,Message
 
+class MessageAdmin(admin.ModelAdmin):
+    readonly_fields=('sender','text','conversation')
+
 admin.site.register(Conversation)
-admin.site.register(Message)
+admin.site.register(Message,MessageAdmin)
