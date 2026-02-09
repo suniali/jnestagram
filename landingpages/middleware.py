6d4792d67a908d46684f50ec9702c414a6f17e31
Jarvis <alimatroky@gmail.com>
2026-02-09 20:47:11 +0330

    update middleware for google

--- landingpages/middleware.py
+++ landingpages/middleware.py
@@ -7,13 +7,24 @@ from .models import LandingPage
 def landingpage_middleware(get_response):
     def middleware(request):
         # Before middleware
+        exempt_urls = [
+            reverse('maintenance'),
+            '/robots.txt',
+            '/sitemap.xml',
+            '/favicon.ico',
+        ]
         if page_is_enabled('Maintenance'):
-            if (request.path != reverse('maintenance')) and ('jnestagram-boss' not in request.path) and STAGING != 'True':
+            is_exempt = any(request.path == url for url in exempt_urls)
+            is_admin = 'jnestagram-boss' in request.path
+
+            if not is_exempt and not is_admin:
                 return HttpResponseRedirect(reverse('maintenance'))
 
         response = get_response(request)
 
         # After middleware
+        if request.path in ['/robots.txt', '/sitemap.xml']:
+            response['X-Robots-Tag'] = 'all'
 
         return response
     return middleware
