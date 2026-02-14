0eaa8f4e17e16b29935f763e350a24eb6db96c3d
Jarvis <alimatroky@gmail.com>
2026-02-14 23:39:08 +0330

    add about page and sotial pages

--- posts/sitemaps.py
+++ posts/sitemaps.py
@@ -7,7 +7,7 @@ class StaticSitemap(Sitemap):
     changefreq = "monthly"
     priority = 1.0
     def items(self):
-        return ['home']
+        return ['home','about']
 
     def location(self, item):
         return reverse(item)
