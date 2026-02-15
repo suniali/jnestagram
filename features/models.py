d6409e0de776fd479d6d675cd7395224f17f3723
Jarvis <alimatroky@gmail.com>
2026-02-15 20:04:09 +0330

    edit feature models.py

--- features/models.py
+++ features/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 class Feature(models.Model):
     name = models.CharField(max_length=255, unique=True,verbose_name=_('Name'))
-    developer = models.CharField(max_length=255, unique=True,verbose_name=_('Developer'))
+    developer = models.CharField(max_length=255,verbose_name=_('Developer'))
     staging_enabled = models.BooleanField(default=False,verbose_name=_('Staging Enabled'))
     production_enabled = models.BooleanField(default=False,verbose_name=_('Production Enabled'))
     created_at = models.DateTimeField(auto_now_add=True,verbose_name=_('Created At'))
