eb80c8f646af6925053184d1d1d34dfcc2d2033d
Jarvis <alimatroky@gmail.com>
2026-02-18 13:54:39 +0330

    add user online count to chats

--- rtchats/consumers.py
+++ rtchats/consumers.py
@@ -17,12 +17,22 @@ class ChatsConsumer(WebsocketConsumer):
             self.chatroom_name,self.channel_name,
         )
 
+        # add or update online users
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
+
         self.accept()
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
             self.chatroom_name,self.channel_name,
         )
+
+        # remove or update online users
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
 
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
@@ -43,7 +53,6 @@ class ChatsConsumer(WebsocketConsumer):
         )
 
 
-
     def message_handler(self,event):
         message_id=event['message_id']
         message=GroupMessage.objects.get(id=message_id)
@@ -52,4 +61,18 @@ class ChatsConsumer(WebsocketConsumer):
             'user': self.user,
         }
         html = render_to_string("rtchats/new_chat_message.html", context=context)
+        self.send(text_data=html)
+
+    def update_online_count(self):
+        online_count=self.chatroom.users_online.count()-1
+
+        event={
+            'type': 'online_count_handler',
+            'online_count': online_count,
+        }
+        async_to_sync(self.channel_layer.group_send)( self.chatroom_name,event )
+
+    def online_count_handler(self,event):
+        online_count=event['online_count']
+        html = render_to_string("rtchats/online_count.html", context={'online_count': online_count})
         self.send(text_data=html)
--- rtchats/models.py
+++ rtchats/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128,unique=True)
+    users_online=models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='online_in_groups',blank=True)
 
     def __str__(self):
         return self.group_name
