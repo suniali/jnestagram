bb6cb9a95f4895fd249204050e1fff73b1ac50ce
Jarvis <alimatroky@gmail.com>
2026-02-22 10:44:57 +0330

    create private chat for users

--- rtchats/models.py
+++ rtchats/models.py
@@ -1,9 +1,13 @@
+import uuid
+
 from django.db import models
 from django.conf import settings
 
 class ChatGroup(models.Model):
-    group_name = models.CharField(max_length=128,unique=True)
+    group_name = models.CharField(max_length=128,unique=True,default=uuid.uuid4)
     users_online=models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='online_in_groups',blank=True)
+    members=models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='chat_groups',blank=True)
+    is_private = models.BooleanField(default=False)
 
     def __str__(self):
         return self.group_name
